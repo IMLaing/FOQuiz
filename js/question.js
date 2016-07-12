12e1396cf0e0c43d1d0e845717ae1d5f51e7f74c
IMLaing <ianmlaing@gmail.com>
2016-07-12 19:31:25 -0400

    added for loop for question.js html display

--- js/question.js
+++ js/question.js
@@ -4,3 +4,12 @@ var Question = function(data){
 	this.answer =	data.answer;
 	this.feedback = data.feedback;
 };
+
+Question.prototype.show = function(){
+	var html = '<h1>' + this.question + '</h1>';
+	for(i = 0; i < this.options.length; i++){
+		html += '<label><input type="radio" name="answer" class="option" value="' + i + '">' + this.options[i] + '</label>';
+	}
+	html += '<input type="button" id="submit" value="Submit Answer">';
+	$(".questionViewer").html(html);
+};
